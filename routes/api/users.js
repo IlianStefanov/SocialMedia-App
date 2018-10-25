2ca534a63db181e40ea07ff4dabe175b5b6e6219
Ilian Stefanov <markuckataa@gmail.com>
2018-10-25 17:43:00 +0300

    Creating Profile schema and Profile form after login

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,11 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
+
+//Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // Load user model
 const User = require("../../models/User");
@@ -17,6 +22,13 @@ router.get("/test", (req, res) => res.json({ msg: "Users Wroks" }));
 // @description     Register users
 // @access          Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       errors.email = "Email already exists";
@@ -53,13 +65,21 @@ router.post("/register", (req, res) => {
 // @access          Public
 
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   //Find User by email
   User.findOne({ email }).then(user => {
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     // Check password
@@ -85,10 +105,27 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Incorrect password" });
+        errors.password = "Incorrect password";
+        return res.status(400).json(errors);
       }
     });
   });
 });
+
+// @route           GET api/users/current
+// @description     Return current user
+// @access          Private
+
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
